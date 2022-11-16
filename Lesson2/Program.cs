c90a4926e5a6f98620b49b7df1f6265ed0294091
Aleksandr <ale240489@yandex.ru>
2022-11-16 11:35:14 +0300

    Work2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт 
+//сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+//N = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+int SumOfNaturalNumFromMToN(int m, int n)
+{
+if(n == m)
+    return n ;
+else
+    return n+SumOfNaturalNumFromMToN(m, n-1);
+}
+Console.WriteLine(SumOfNaturalNumFromMToN(4, 8));
